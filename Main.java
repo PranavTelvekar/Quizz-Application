2120647335bc2cd28b401d4649abdf11a3e56e1e
PranavTelvekar <pranavtelvekar9401@gmail.com>
2026-02-06 15:15:25 +0530

    Now I Have Done To Diplay Quetions Properly
    Taking Answers One By One

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
 
         QuetionService service=new QuetionService();
 
-        service.displayQuetions();
+        service.playQuizz();
     }
 }
--- QuetionService.java
+++ QuetionService.java
@@ -1,18 +1,32 @@
+import java.util.Scanner;
 public class QuetionService {
     Quetion[] quetions=new Quetion[5];
-
+    String[] selection=new String[5];
     public QuetionService(){
         quetions[0]=new Quetion(1,"what1","java","CPP","python","javascript","java");
-        quetions[1]=new Quetion(1,"what2","java","CPP","python","javascript","java");
-        quetions[2]=new Quetion(1,"what3","java","CPP","python","javascript","java");
-        quetions[3]=new Quetion(1,"what4","java","CPP","python","javascript","java");
-        quetions[4]=new Quetion(1,"what5","java","CPP","python","javascript","java");
+        quetions[1]=new Quetion(2,"what2","java","CPP","python","javascript","java");
+        quetions[2]=new Quetion(3,"what3","java","CPP","python","javascript","java");
+        quetions[3]=new Quetion(4,"what4","java","CPP","python","javascript","java");
+        quetions[4]=new Quetion(5,"what5","java","CPP","python","javascript","java");
     }
 
-    public void displayQuetions(){
-        
+    public void playQuizz(){
+        int i=0;
         for(Quetion q:quetions){
-            System.out.println(q);
+            System.out.println("Quetion no: "+q.getId());
+            System.out.println(q.getQuetion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+
+            Scanner sc=new Scanner(System.in);
+            selection[i++]=sc.nextLine();
+
+        }
+
+        for(String s:selection){
+            System.out.println(s);
         }
     }
 }
